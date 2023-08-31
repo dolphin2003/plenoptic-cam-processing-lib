6e13023759e43835b0c38436b05767741538601f
dolphin2003 <dolphin2003@proton.me>
2023-08-31 11:50:21 +0000

    More bugs

--- pleno/graphic/display.h
+++ pleno/graphic/display.h
@@ -271,4 +271,166 @@ inline void display(
 		);
 		const auto p = reproject_miccenter(model, c);
 		RENDER_DEBUG_2D(
-  			Viewer::context().laye
+  			Viewer::context().layer(layer+1)
+  				.name("Reprojected Center")
+  				.pen_color(v::cyan).pen_width(1),
+  			p
+		);		
+	}
+	Viewer::update();
+	Viewer::update();
+
+//For each frame 
+	for(auto & [f, ob] : obs)
+	{
+		if (usePictures)
+		{
+			try { pictures.at(f); }
+			catch (std::out_of_range&) { continue; }
+		}
+		PRINT_DEBUG("Display information of frame f = " << f);
+		
+		for(const auto& [p,f] : poses) if(f == ob[0].frame) model.pose() = p;
+		display(model);
+		
+		if (usePictures)
+		{
+			RENDER_DEBUG_2D(
+				Viewer::context().layer(Viewer::layer()).name("Frame f = "+std::to_string(f)), 
+				pictures.at(f)
+			);
+		
+			Viewer::update();
+		}
+		
+		//get theorical reprojection
+		Observations tbaps; int cluster = 0;
+		for (const auto& pw : grid)
+		{
+			const P3D pc = to_coordinate_system_of(model.pose(), pw); // CAMERA
+			
+			Observations temp;
+			mfpc.project(pc, temp);
+			for (auto& o : temp) { o.frame = f; o.cluster = cluster; }
+			
+			tbaps.insert(tbaps.end(), std::make_move_iterator(temp.begin()), std::make_move_iterator(temp.end()));
+			++cluster;
+		}
+		
+		const Viewer::Layer layer = Viewer::layer();
+		for (const auto& o : ob)
+		{				
+			RENDER_DEBUG_2D(
+	  			Viewer::context().layer(layer)
+	  				.name("BAP ("+std::to_string(f)+")")
+	  				.point_style(v::Round)
+	  				.pen_color(palette[o.cluster]).pen_width(2),
+	  			Disk{o[0], o[1], (std::is_same_v<Observations, BAPObservations>?o[2]:0.0)}
+			);
+			
+			const P2D corner = reproject_corner(model, model.pose(), grid, o);
+			const double radius = (mfpc.I() > 0u and std::is_same_v<Observations, BAPObservations>) ? reproject_radius(model, model.pose(), grid, o) : 0.0;
+
+			RENDER_DEBUG_2D(
+	  			Viewer::context().layer(layer+1)
+	  				.name("Reprojected BAP ("+std::to_string(f)+")")
+					.point_style(v::Cross)
+	  				.pen_color(palette[o.cluster+1]).pen_width(2),
+	  			Disk{corner, radius}
+			);
+		}
+		
+		Viewer::update();
+		Viewer::update();
+		
+		for (const auto& o : tbaps)
+		{
+			RENDER_DEBUG_2D(
+	  			Viewer::context().layer(Viewer::layer())
+	  				.name("Theorical BAP ("+std::to_string(f)+")")
+	  				.point_style(v::Cross)
+	  				.pen_style(v::DashLine)
+	  				.pen_color(palette[o.cluster]).pen_width(2),
+	  			Disk{o[0], o[1], (std::is_same_v<Observations, BAPObservations>?o[2]:0.0)}
+			);
+		}
+
+		Viewer::context().point_style(v::Pixel).pen_style(v::SolidLine); //restore point style
+		Viewer::update();
+		
+		wait();
+	}
+}
+
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+inline void display(const DepthMap& dm, const PlenopticCamera& pcm)
+{
+GUI(	
+	std::string ss = (dm.is_virtual_depth()?"":"Metric ");
+	
+	const DepthMapImage dmi = DepthMapImage{dm, pcm};
+	
+	Image idm; cv::cvtColor(dmi.image, idm, cv::COLOR_BGR2RGB);
+	RENDER_DEBUG_2D(
+		Viewer::context().layer(Viewer::layer()++)
+			.name(ss+"Depth Map"),
+		idm
+  	);
+
+  	// the legend
+	constexpr int H = 50;
+	const int W = idm.cols;
+	
+	cv::Mat legend;
+	cv::resize(dmi.colormap, legend, cv::Size(W, H), 0, 0, cv::INTER_AREA);
+	cv::imwrite("cm-legend.png", legend);
+	cv::cvtColor(legend, legend, cv::COLOR_BGR2RGB);
+	
+	ss = (dm.is_virtual_depth()?"":" (mm)");
+	RENDER_DEBUG_2D(
+		Viewer::context().layer(Viewer::layer())
+			.name("Legend"+ss),
+		legend,
+		0, -2 * H
+  	);
+  	
+  	const double nbsample = dm.is_virtual_depth()? std::ceil(dm.max_depth() - dm.min_depth()) : 15.;
+  	const double stepv = dm.is_virtual_depth()? 1. : (dm.max_depth() - dm.min_depth()) / nbsample;
+  	const double steppix = W / nbsample;
+  	for (double v = dm.min_depth(), offsetpix = -5.; v <= dm.max_depth(); v += stepv, offsetpix += steppix)
+  	{
+  		std::ostringstream oss;
+  		oss.precision((dm.is_virtual_depth()?2:4));
+  		oss << v;
+  		
+  		Viewer::context().layer(Viewer::layer())
+  			.font_size(50)
+  			.pen_color(v::white)
+  			.add_text(
+  				offsetpix, - 2.5 * H,
+  				oss.str() 			
+  			);	  	
+  	}
+	Viewer::context().layer(Viewer::layer()++).update();  
+	
+	if (dm.is_coarse_map())
+	{	
+		constexpr std::size_t BORDER_MARGIN = 0;
+
+		const std::size_t kmax = dm.width() - BORDER_MARGIN; 
+		const std::size_t kmin = 0 + BORDER_MARGIN;
+		const std::size_t lmax = dm.height() - BORDER_MARGIN; 
+		const std::size_t lmin = 0 + BORDER_MARGIN;
+	
+		ss = (dm.is_virtual_depth()?"":"Metric ");  	
+		for(std::size_t k = kmin; k < kmax; ++k)
+		{
+			for(std::size_t l = lmin; l < lmax; ++l)
+			{
+				const auto center = pcm.mia().nodeInWorld(k,l);	
+				std::ostringstream oss;
+		  		oss.precision((dm.is_virtual_depth()?3:4));
+		  		oss << dm.depth(k,l)*(dm.is_virtual_de
