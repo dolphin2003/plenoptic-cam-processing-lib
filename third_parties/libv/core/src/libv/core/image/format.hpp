2f4b6c5909a6e6f5f9173c8c1026072fea7e9043
dolphin2003 <dolphin2003@proton.me>
2023-10-24 19:32:38 +0000

    site updates

--- third_parties/libv/core/src/libv/core/image/format.hpp
+++ third_parties/libv/core/src/libv/core/image/format.hpp
@@ -30,4 +30,49 @@ namespace core {
 /// \{
 
 /// The format of an Image.
-/// This class encapsulates the template parameters used in the 
+/// This class encapsulates the template parameters used in the Image class, so that the Image class only takes one template parameter.
+template<class _scalar_type, int _flags, int _row_count, int _column_count, int _plane_count, int _row_step, int _column_step>
+struct ImageFormat
+{
+  static const int
+    /// The row count at compile time.
+    row_count = _row_count,
+    /// The column count at compile time.
+    column_count = _column_count,
+    /// The plane count at compile time.
+    plane_count = _plane_count,
+    /// The row step at compile time.
+    row_step = _row_step,
+    /// The column step at compile time.
+    column_step = _column_step,
+    /// The flags at compile time.
+    flags = _flags,
+    /// The flags at compile time, without the semantic-related ones.
+    naked_flags = flags & ~IMAGE_SEMANTIC_MASK,
+    /// True if the image has value semantic.
+    has_value_semantic = (flags & IMAGE_SEMANTIC_MASK) == IMAGE_SEMANTIC_VALUE,
+    /// True if the image has pointer semantic.
+    has_pointer_semantic = (flags & IMAGE_SEMANTIC_MASK) == IMAGE_SEMANTIC_POINTER,
+    /// True if the image has reference semantic.
+    has_reference_semantic = (flags & IMAGE_SEMANTIC_MASK) == IMAGE_SEMANTIC_REFERENCE,
+    /// The byte count at compile time.
+    byte_count = row_count * (row_step < 0 ? column_count : row_step) * plane_count * has_value_semantic,
+    /// The dimension.
+    dimension = (row_count != 1) + (column_count != 1) + (plane_count != 1);
+
+  /// \copydoc Image::scalar_type
+  typedef _scalar_type scalar_type;
+
+  /// \copydoc Image::with_scalar_type
+  template<class _new_scalar_type>
+  struct with_scalar_type
+  {
+    /// \copydoc Image::with_scalar_type::type
+    typedef ImageFormat<_new_scalar_type, flags, row_count, column_count, plane_count, row_step, column_step> type;
+  };
+};
+
+/// \}
+}}
+
+#endif
