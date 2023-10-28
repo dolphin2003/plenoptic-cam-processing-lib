b3ab8469a77ed957628b3ca84094bdf20a3ae483
dolphin2003 <dolphin2003@proton.me>
2023-10-28 20:51:54 +0000

    3.3.8

--- third_parties/libv/core/src/libv/core/serialization/archives/yaml.cpp
+++ third_parties/libv/core/src/libv/core/serialization/archives/yaml.cpp
@@ -92,4 +92,212 @@ static void save_(YAML::Emitter &out, const char *&values, const size_t *extents
 }
 
 template<class T>
-static void save_recursive(YAML::Emitter &out, const T *&values, const size_t *
+static void save_recursive(YAML::Emitter &out, const T *&values, const size_t *extents, size_t rank)
+{
+  out << YAML::BeginSeq;
+  for(size_t i = 0; i < *extents; ++i)
+  {
+    save_(out, values, extents + 1, rank - 1);
+  }
+  out << YAML::EndSeq;
+}
+
+static void check_format(std::string file)
+{
+  file = file.substr(file.rfind('.') + 1);
+  transform(file.begin(), file.end(), file.begin(), tolower);
+  if(file != "yaml" && file != "yml") throw std::logic_error(file);
+}
+
+static struct load_yaml: register_codec<const std::string, Serializable> {
+  void operator()(const std::string &input, Serializable &output) const {
+    check_format(input);
+    std::ifstream stream(input.c_str());
+    YamlInputArchive archive(stream);
+    v::load(archive, output);
+  }} load_yaml;
+
+static struct save_yaml: register_codec<const Serializable, const std::string> {
+  void operator()(const Serializable &input, const std::string &output) const {
+    check_format(output);
+    std::ofstream stream(output.c_str());
+    YamlOutputArchive archive(stream);
+    v::save(archive, input);
+  }} save_yaml;
+
+}
+
+/**
+  Initialize this archive with a stream.
+*/
+YamlInputArchive::YamlInputArchive
+( std::istream &stream ///< A reference to a stream.
+)
+{
+  nodes.emplace(YAML::Load(stream));
+}
+
+bool YamlInputArchive::begin(const std::string &key)
+{
+  assert(!nodes.empty());
+  assert(nodes.top().IsMap());
+  nodes.emplace(nodes.top()[key]);
+  if(nodes.top())
+  {
+    return true;
+  }
+  else
+  {
+    nodes.pop();
+    return false;
+  }
+}
+
+bool YamlInputArchive::begin(std::string *key)
+{
+  assert(!nodes.empty());
+  assert(!iterators.empty());
+  assert(nodes.top().IsMap());
+  assert(iterators.top() != nodes.top().end());
+  *key = iterators.top()->first.Scalar();
+  return begin();
+}
+
+bool YamlInputArchive::begin()
+{
+  assert(!nodes.empty());
+  assert(!iterators.empty());
+  assert(nodes.top().IsMap() || nodes.top().IsSequence());
+  assert(iterators.top() != nodes.top().end());
+  nodes.emplace(nodes.top().IsMap() ? iterators.top()->second : *iterators.top());
+  ++iterators.top();
+  return true;
+}
+
+void YamlInputArchive::end()
+{
+  assert(!nodes.empty());
+  nodes.pop();
+}
+
+/**
+  Il faut impérativement utiliser `node.reset()`. On ne peut pas simplement faire `node = node[0]` parce que ça détruirait le contenu de `node`.
+*/
+void YamlInputArchive::get_extents(size_t *extents, size_t rank)
+{
+  assert(!nodes.empty());
+  YAML::Node node(nodes.top());
+  for(size_t i = 0; i < rank; ++i)
+  {
+    if(node.IsMap())
+    {
+      extents[i] = node.size();
+      node.reset(node.begin()->second);
+    }
+    else if(node.IsSequence())
+    {
+      extents[i] = node.size();
+      node.reset(node[0]);
+    }
+    else if(node.IsScalar())
+    {
+      assert(rank < 2);
+      extents[i] = node.Scalar().size();
+      node.reset();
+    }
+  }
+}
+
+void YamlInputArchive::begin_list()
+{
+  assert(!nodes.empty());
+  iterators.emplace(nodes.top().begin());
+}
+
+void YamlInputArchive::end_list()
+{
+  assert(!iterators.empty());
+  iterators.pop();
+}
+
+/**
+  Initialize this archive with a stream.
+*/
+YamlOutputArchive::YamlOutputArchive
+( std::ostream &stream ///< A reference to a stream.
+)
+: out(stream)
+{
+  state.emplace(YAML::Value);
+}
+
+void YamlOutputArchive::begin(const std::string &key)
+{
+  assert(!state.empty());
+  assert(state.top() == YAML::BeginMap || state.top() == YAML::Key || state.top() == YAML::Value);
+  if(state.top() != YAML::BeginMap)
+  {
+    out << YAML::BeginMap;
+    state.top() = YAML::BeginMap;
+  }
+  out << YAML::Key << key;
+  state.emplace(YAML::Key);
+}
+
+void YamlOutputArchive::begin()
+{
+  assert(!state.empty());
+  assert(state.top() == YAML::BeginSeq || state.top() == YAML::Key || state.top() == YAML::Value);
+  if(state.top() != YAML::BeginSeq)
+  {
+    out << YAML::BeginSeq;
+    state.top() = YAML::BeginSeq;
+  }
+  state.emplace(YAML::Auto);
+}
+
+void YamlOutputArchive::end()
+{
+  assert(!state.empty());
+  assert(state.top() == YAML::BeginMap || state.top() == YAML::BeginSeq || state.top() == YAML::Key || state.top() == YAML::Value);
+  if(state.top() == YAML::BeginMap)
+  {
+    out << YAML::EndMap;
+  }
+  if(state.top() == YAML::BeginSeq)
+  {
+    out << YAML::EndSeq;
+  }
+  if(state.top() == YAML::Key)
+  {
+    // Si on a une clé mais pas de valeur, il faut ajouter une valeur vide.
+    // Sinon le fichier se casse quand on essaie d'enregistrer des listes vides.
+    out << YAML::Null;
+  }
+  state.pop();
+}
+
+void YamlOutputArchive::begin_list(size_t)
+{
+}
+
+void YamlOutputArchive::end_list()
+{
+}
+
+/**
+  Define overloads for each fundamental type.
+*/
+#define _(T)\
+\
+  void YamlInputArchive::serialize(T &value)\
+  {\
+    load_(nodes.top(), value);\
+  }\
+\
+  void YamlInputArchive::serialize(T *values, const size_t *extents, size_t rank)\
+  {\
+    load_(nodes.top(), values, extents, rank);\
+  }\
+\
+  void YamlOutputArchive::serialize
