28db8e62a7f53bef867ef2dda38535b655cdbdb3
dolphin2003 <dolphin2003@proton.me>
2023-11-07 15:21:18 +0000

    Link updates

--- third_parties/libv/core/tests/rwlock.cpp
+++ third_parties/libv/core/tests/rwlock.cpp
@@ -44,3 +44,70 @@ namespace
   int write(int x, unsigned ms)
   {
     RwLock::Writer w(rwlock);
+    const int old = shared;
+    sleep_ms(ms);
+    shared += x;
+    return old;
+  }
+
+  void fail(const std::string &s)
+  {
+    std::cerr << s << std::endl;
+    exit(1);
+  }
+
+  void check(bool b, const std::string &s)
+  {
+    if (!b)
+      fail(s);
+  }
+
+  /*
+   * SCENARIO
+   * 0    R1 and R2 start reading
+   * 500  W1 tries to acquire the lock and gets blocked
+   * 1000 W2, R1 and R2 try to acquire the lock, one of the writers gets it
+   * 2000 First writer (either W1 or W2) done, now the 2nd writer gets the lock
+   * 3000 2nd writer done, both readers acquire the lock
+   * 4000 Both readers release the lock
+   */
+
+  void reader1()
+  {
+    check(read(1000) == 0, "R1_1");
+    check(read(1000) == 11, "R1_2");
+    check(elapsed_ms() >= 4000 && elapsed_ms() < 4300, "R1_3");
+  }
+
+  void reader2()
+  {
+    check(read(1000) == 0, "R2_1");
+    check(read(1000) == 11, "R2_2");
+    check(elapsed_ms() >= 4000 && elapsed_ms() < 4300, "R2_3");
+  }
+
+  void writer1()
+  {
+    sleep_ms(500);
+    write(1, 1000);
+  }
+
+  void writer2()
+  {
+    sleep_ms(1000);
+    write(10, 1000);
+  }
+}
+
+int main()
+{
+  std::thread r1(reader1);
+  std::thread r2(reader2);
+  std::thread w1(writer1);
+  std::thread w2(writer2);
+
+  w2.join();
+  w1.join();
+  r2.join();
+  r1.join();
+}
---
+++ third_parties/libv/geometry/src/libv/geometry/euler_angles/euler_angles.hpp
@@ -0,0 +1,9 @@
+/**
+
+\file
+Definition of the EulerAngles class.
+\author Clement Deymier (2012)
+\copyright 2012 Institut Pascal
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Pu
