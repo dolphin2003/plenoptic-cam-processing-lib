22d80a3a0e06524e700ec161817bbbd013d29038
dolphin2003 <dolphin2003@proton.me>
2023-11-18 12:56:44 +0000

    ext4: move_extent code cleanup

--- third_parties/libv/graphic/src/libv/graphic/viewer.cpp
+++ third_parties/libv/graphic/src/libv/graphic/viewer.cpp
@@ -228,3 +228,417 @@ void Viewer::zoom(float zoom)
 
 void Viewer::look_at(QPointF point)
 {
+  QPointF d = QPointF(width(), height()) / 2 - that->transform.map(point);
+  that->transform *= QTransform::fromTranslate(d.x(), d.y());
+  that->update();
+}
+
+/// Save the current state of this viewer as an image.
+void
+Viewer::save_screenshot(void)
+{
+  QString file = QFileDialog::getSaveFileName(this, "Save Screen", "", "Images (*.jpg *.png *.pdf *.svg)");
+  QString suffix = QFileInfo(file).suffix().toLower();
+
+  if(suffix == "pdf")
+  {
+    QPrinter printer;
+    printer.setOutputFormat(QPrinter::PdfFormat);
+    printer.setOutputFileName(file);
+    printer.setPaperSize(size(), QPrinter::DevicePixel);
+    printer.setFullPage(true);
+    paint(&printer);
+  }
+  else if(suffix == "svg")
+  {
+    QSvgGenerator printer;
+    printer.setFileName(file);
+    printer.setSize(size());
+    paint(&printer);
+  }
+  else
+  {
+    if(suffix.isEmpty()) file += ".png";
+    const QPixmap image = QPixmap::grabWidget(this);
+    image.save(file);
+  }
+}
+
+/// Save the upcoming states of this viewer as a video.
+void
+Viewer::save_video(bool value)
+{
+  if(value)
+  {
+    that->video_directory = QFileDialog::getExistingDirectory(this, that->action_save_video->text(), QString(), QFileDialog::ShowDirsOnly);
+    if(that->video_directory.isNull())
+    {
+      that->action_save_video->setChecked(false);
+    }
+    else
+    {
+      that->video_counter = 0;
+      that->video_timer = startTimer(100);
+    }
+  }
+  else
+  {
+    killTimer(that->video_timer);
+  }
+}
+
+/// Map a point from scene coordinates to viewport coordinates.
+QPoint
+Viewer::from_scene(qreal x, qreal y) const
+{
+  return that->transform.map(QPointF(x, y)).toPoint();
+}
+
+/// Map a rectangle from scene coordinates to viewport coordinates.
+QRect
+Viewer::from_scene(const QRectF &rect) const
+{
+  return that->transform.mapRect(rect).toRect();
+}
+
+/// Map a rectangle from viewport coordinates to scene coordinates.
+QRectF
+Viewer::to_scene(const QRect &rect) const
+{
+  return that->transform.inverted().mapRect(rect);
+}
+
+/// Map a point from viewport coordinates to scene coordinates.
+QPointF
+Viewer::to_scene(const QPoint &p) const
+{
+  return that->transform.inverted().map(p);
+}
+
+/// The bounding rectangle of the scene.
+QRectF
+Viewer::bounds(void) const
+{
+  QRectF rect;
+  for(LayerList::const_reference layer: that->layers)
+  {
+    rect |= layer.second.bounds;
+  }
+  return rect;
+}
+
+void
+Viewer::_show(bool value)
+{
+  that->layers[static_cast<const QAction *>(sender())->data().toInt()].visible = value;
+}
+
+/// \name QAbstractScrollArea Interface
+/// \{
+
+void Viewer::scrollContentsBy(int dx, int dy)
+{
+  that->controller().on_scroll(that, dx, dy);
+}
+
+void Viewer::closeEvent(QCloseEvent *)
+{
+  that->action_save_video->setChecked(false);
+}
+
+void Viewer::mouseMoveEvent(QMouseEvent *event)
+{
+  if(!that->mouse_dragging && (event->pos() - that->mouse_pos).manhattanLength() >= QApplication::startDragDistance())
+  {
+    that->mouse_dragging = true;
+    setCursor(Qt::ClosedHandCursor);
+  }
+  if(that->mouse_dragging)
+  {
+    switch(that->mouse_buttons)
+    {
+      case Qt::RightButton:
+      that->controller().on_drag_right(that, event);
+      break;
+
+      default:
+      that->controller().on_drag(that, event);
+    }
+    that->mouse_pos = event->pos();
+  }
+}
+
+void Viewer::mousePressEvent(QMouseEvent *event)
+{
+  that->mouse_dragging = false;
+  that->mouse_pos = event->pos();
+  that->mouse_buttons = event->buttons();
+}
+
+void Viewer::mouseReleaseEvent(QMouseEvent *event)
+{
+  setCursor(Qt::ArrowCursor);
+  if(!that->mouse_dragging)
+  {
+    switch(that->mouse_buttons)
+    {
+      case Qt::RightButton:
+      that->controller().on_click_right(that, event);
+      break;
+
+      default:
+      that->controller().on_click(that, event);
+    }
+  }
+}
+
+// for delta = 120 (15° rotation) we want to zoom by 20%
+// s = 1.2^(delta / 120) = (1.2^(1/120))^delta
+void Viewer::wheelEvent(QWheelEvent* event)
+{
+  that->controller().on_wheel(that, event);
+}
+
+void Viewer::resizeEvent(QResizeEvent *)
+{
+  horizontalScrollBar()->setPageStep(viewport()->width());
+  horizontalScrollBar()->setSingleStep(viewport()->width() / 20);
+  verticalScrollBar()->setPageStep(viewport()->height());
+  verticalScrollBar()->setSingleStep(viewport()->height() / 20);
+  that->update();
+}
+
+void Viewer::keyPressEvent(QKeyEvent *event)
+{
+  switch(event->key())
+  {
+    case Qt::Key_Up:
+    if(reticle_is_visible())
+    {
+      if(reticle_pos().y() > 0)
+      {
+        reticle_pos(reticle_pos() - QPoint(0, 1));
+      }
+    }
+    break;
+
+    case Qt::Key_Down:
+    if(reticle_is_visible())
+    {
+      if(reticle_pos().y() < viewport()->height())
+      {
+        reticle_pos(reticle_pos() + QPoint(0, 1));
+      }
+    }
+    break;
+
+    case Qt::Key_Left:
+    if(reticle_is_visible())
+    {
+      if(reticle_pos().x() > 0)
+      {
+        reticle_pos(reticle_pos() - QPoint(1, 0));
+      }
+    }
+    break;
+
+    case Qt::Key_Right:
+    if(reticle_is_visible())
+    {
+      if(reticle_pos().x() < viewport()->width())
+      {
+        reticle_pos(reticle_pos() + QPoint(1, 0));
+      }
+    }
+    break;
+  }
+}
+
+void Viewer::paintEvent(QPaintEvent *)
+{
+  static_cast<QGLWidget *>(viewport())->makeCurrent();
+  const int width = viewport()->width(), height = viewport()->height();
+
+  glViewport(0, 0, width, height);
+  glMatrixMode(GL_PROJECTION);
+  that->controller().apply(that);
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+
+  double r, g, b, a;
+  that->background_brush.color().getRgbF(&r, &g, &b, 0);
+  glClearColor(r, g, b, 1);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  // ---------- DRAW 3D OBJECTS
+
+  for(LayerList::const_reference layer: that->layers)
+  {
+    if(!layer.second.visible)
+    {
+      continue;
+    }
+
+    for(ContextList::const_reference context: layer.second.data)
+    {
+      (context.first.point_style == Round && context.first.pen.width() > 1 ? glEnable : glDisable)(GL_POINT_SMOOTH);
+      context.first.pen.color().getRgbF(&r, &g, &b, &a);
+      glPointSize(context.first.pen.width());
+      glLineWidth(context.first.pen.width());
+      glColor4d(r, g, b, a);
+
+      for(const std::function<void()> &p: context.second.functions)
+      {
+        p();
+      }
+    }
+  }
+
+  // ---------- DRAW 2D OBJECTS
+
+  paint(viewport());
+}
+
+void Viewer::paint(QPaintDevice *device)
+{
+  QPainter painter(device);
+  painter.setRenderHints(0);
+  painter.setTransform(that->transform);
+
+  for(LayerList::const_reference layer: that->layers)
+  {
+    if(!layer.second.visible)
+    {
+      continue;
+    }
+
+    for(ContextList::const_reference context: layer.second.data)
+    {
+      painter.setPen(context.first.pen);
+      painter.setBrush(context.first.brush);
+      painter.setFont(context.first.font);
+
+      // ---------- DRAW POINTS
+
+      switch(context.first.point_style)
+      {
+        case Pixel:
+        case Round:
+        painter.drawPoints(context.second.points);
+        break;
+
+        case Cross:
+        for(const QPointF &p: context.second.points)
+        {
+          static const QPointF x(5, 0), y(0, 5);
+          painter.drawLine(p - x, p + x);
+          painter.drawLine(p - y, p + y);
+        }
+        break;
+      }
+
+      // ---------- DRAW LINES
+
+      painter.drawLines(context.second.lines);
+
+      // ---------- DRAW RECTANGLES
+
+      painter.drawRects(context.second.rectangles);
+
+      // ---------- DRAW POLYGONS
+
+      for(QPolygonF p: context.second.polygons)
+      {
+        painter.drawPolygon(p);
+      }
+
+      // ---------- DRAW ELLIPSES
+
+      for(const Data::Ellipse &p: context.second.ellipses)
+      {
+        painter.drawEllipse(p.center, p.rx, p.ry);
+      }
+
+      // ---------- DRAW IMAGES
+
+      for(const Data::Image &p: context.second.images)
+      {
+        painter.drawImage(p.position, p.image);
+      }
+
+      // ---------- DRAW TEXTS
+
+      switch (context.first.font_direction)
+      {
+        case Horizontal:
+        for(const Data::Text &p: context.second.texts)
+        {
+          painter.drawText(p.position, p.text);
+        }
+        break;
+
+        case Vertical:
+        for(const Data::Text &p: context.second.texts)
+        {
+          painter.save();
+          painter.translate(p.position);
+          painter.rotate(90);
+          painter.drawText(QPointF(), p.text);
+          painter.restore();
+        }
+        break;
+      }
+
+      // ---------- DRAW ARROWS
+
+      for(const Data::Arrow &p: context.second.arrows)
+      {
+        static const qreal arrow_size = 10, angle = M_PI * 11 / 12;
+        painter.drawLine(p.point, p.point + QPointF(std::cos(p.angle + angle), std::sin(p.angle + angle)) * arrow_size);
+        painter.drawLine(p.point, p.point + QPointF(std::cos(p.angle - angle), std::sin(p.angle - angle)) * arrow_size);
+      }
+    }
+  }
+
+  // ---------- DRAW AXES
+
+  painter.resetTransform();
+
+  if(that->axes_enabled)
+  {
+    painter.setPen(that->axes_pen);
+    painter.setFont(qApp->font());
+    QFontMetrics metrics = painter.fontMetrics();
+
+    switch(that->axes_type)
+    {
+      case AxesUV:
+      that->draw_axes_uv(painter, metrics);
+      break;
+
+      case AxesXY:
+      that->draw_axes_xy(painter, metrics);
+      break;
+    }
+  }
+
+  // ---------- DRAW RETICLE
+
+  if(that->reticle_enabled)
+  {
+    painter.setPen(that->reticle_pen);
+    painter.drawLine(0, that->reticle_pos.y(), viewport()->width(), that->reticle_pos.y());
+    painter.drawLine(that->reticle_pos.x(), 0, that->reticle_pos.x(), viewport()->height());
+  }
+}
+
+void Viewer::timerEvent(QTimerEvent *)
+{
+  const QPixmap image = QPixmap::grabWidget(this);
+  const QString file = QString("%1/image_%2.png").arg(that->video_directory).arg(that->video_counter);
+  image.save(file);
+  ++that->video_counter;
+}
+
+/// \}
+}}
