4c883f7702b9cde4b8f285122e83b8cd90d2b657
dolphin2003 <dolphin2003@proton.me>
2023-11-19 10:25:30 +0000

    1.0.32

---
+++ third_parties/libv/graphic/src/private/TopicDisplay.cpp
@@ -0,0 +1,16 @@
+/**
+
+\file
+Définition de la classe v::graphic::TopicDisplay, dont des plugins rviz pourront dériver.
+Les déclarations sont dans TopicDisplay.hpp.
+
+\author Alexis Wilhelm (2018)
+
+*/
+
+/**
+
+\class v::graphic::TopicDisplay
+Une classe à dériver pour faciliter l'écriture de plugins pour rviz.
+
+rviz propose une classe rviz::MessageFilterDisplay pour faciliter l'écriture des plugins q
