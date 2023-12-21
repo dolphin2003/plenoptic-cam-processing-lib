97a388b43dbe76c645dd7710759a7491966103dc
dolphin2003 <dolphin2003@proton.me>
2023-12-21 19:43:38 +0000

    update changes docs

--- third_parties/lma/tests/simple32.cpp
+++ third_parties/lma/tests/simple32.cpp
@@ -1,3 +1,4 @@
+
 #include <libv/lma/numeric/divers.hpp>
 #include <libv/lma/lma.hpp>
 
@@ -7,7 +8,6 @@ typedef double type;
 struct A : Eigen::Matrix<type,1,1> {};
 struct B : Eigen::Matrix<type,1,1> {};
 struct C : Eigen::Matrix<type,1,1> {};
-
 
 namespace ttt
 {
@@ -33,48 +33,36 @@ namespace ttt
       return true;
     }
   };
-  
+
 namespace lma
 {
   template<> struct Size<A> { static const size_t value = 1; };
   template<> struct Size<B> { static const size_t value = 1; };
   template<> struct Size<C> { static const size_t value = 1; };
-
-  void apply_increment(A& a, double h, const Adl&)
-  {
-    a[0] += h;
-  }
-  void apply_increment(B& a, double h, const Adl&)
-  {
-    a[0] += h;
-  }
-  void apply_increment(C& a, double h, const Adl&)
-  {
-    a[0] += h;
-  }
 };
+
 
 template<class Solver, class AlgoTag, class V0, class V1, class V2> void solve(V0 v0, V1 v1, V2 v2)
 {
-  Solver bundle(1,5);
+  Solver bundle(1,1);
 
-  for(size_t i = 0 ; i < v1.size() ; ++i)
-    for(size_t j = 0 ; j < v2.size() ; ++j)
-	bundle.add(F1(),&v1[i],&v2[j]);
-  
+  for(size_t j = 0 ; j < v1.size() ; ++j)
+    for(size_t k = 0 ; k < v2.size() ; ++k)
+      bundle.add(F1(),&v1[j],&v2[k]);
+      
   for(size_t i = 0 ; i < v0.size() ; ++i)
       for(size_t k = 0 ; k < v2.size() ; ++k)
         bundle.add(F2(),&v0[i],&v2[k]);
-      
-    bundle.solve(AlgoTag(),lma::enable_verbose_output());
+  
+  bundle.solve(AlgoTag(),lma::minimal_verbose());
 }
 
 
 int main()
 {
-  size_t size0 = 30;
-  size_t size1 = 10;
-  size_t size2 = 100;
+  size_t size0 = 1;
+  size_t size1 = 1;
+  size_t size2 = 1;
 
   std::vector<A,Eigen::aligned_allocator<A>> v0(size0);
   std::vector<B,Eigen::aligned_allocator<B>> v1(size1);
@@ -89,8 +77,8 @@ int main()
   for(auto& x : v2)
     x << random(1.0);
 
-  typedef Solver<F1,F2> Solver;
-  solve<Solver,DENSE_SCHUR2_>(v0,v1,v2);
+  typedef Solver<mpl::vector<A,B,C>,F1,F2> Solver;
 
+  solve<Solver,DENSE_SCHUR_>(v0,v1,v2);
   return 0;
 }
---
+++ third_parties/lma/tests/ttt.cpp
@@ -0,0 +1,8 @@
+#include <boost/mpl/vector.hpp>
+#include <libv/lma/ttt/mpl/naming.hpp>
+#include <iostream>
+
+
+int main()
+{
+  std::cout << " --> " << ttt::name<boost::mpl::vector<int,char,
